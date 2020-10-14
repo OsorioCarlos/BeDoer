4b82a125f1ed80c81dbd8829777777a429d7b16b
JoelTapia99 <joelts159@gmail.com>
2020-10-14 10:17:19 -0500

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
+// dependencias de angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// importacion del routing
+import { AppRoutingModule } from './app-routing.module';
+
+// componentes de mi app
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BoardTaskComponent } from './components/board-task/board-task.component';
@@ -16,7 +21,8 @@ import { BoardTaskTeamComponent } from './components/board-task-team/board-task-
     BoardTaskTeamComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
