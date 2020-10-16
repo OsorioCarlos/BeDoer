9c2a8cbfcfb313a0a7a0675486c7c7beac15577e
JaviPtz <jam.pastaz@yavirac.deu.ec>
2020-10-16 10:55:36 -0500

    interfaz board-task-team

--- src/app/components/board-task-team/board-task-team.component.ts
+++ src/app/components/board-task-team/board-task-team.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare let $: any;
+
 @Component({
   selector: 'app-board-task-team',
   templateUrl: './board-task-team.component.html',
@@ -12,4 +14,13 @@ export class BoardTaskTeamComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  tarea(){
+    $('#tarea').modal();
+  }
+  salir()
+  {
+    setTimeout(() => {
+      $('#tarea').modal('hide');
+    },300);
+  }
 }
