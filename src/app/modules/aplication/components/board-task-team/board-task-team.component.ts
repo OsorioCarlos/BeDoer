5365ffe84f0fd0786bee8486dabcc840e759ef0c
MasterJ <joelts159@gmail.com>
2021-04-26 20:56:40 -0500

    crud finished in task-team

--- src/app/modules/aplication/components/board-task-team/board-task-team.component.ts
+++ src/app/modules/aplication/components/board-task-team/board-task-team.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
-import { CategoryService } from '../../../../services/category.service';
-import { FormBuilder } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
-import { AplicationService } from '../../../../services/aplication/aplication.service';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
+import {CategoryService} from '../../../../services/category.service';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ToastrService} from 'ngx-toastr';
+import {AplicationService} from '../../../../services/aplication/aplication.service';
 
 declare let $: any;
 
@@ -15,15 +15,15 @@ declare let $: any;
 })
 export class BoardTaskTeamComponent implements OnInit {
 
-   // -------------------------------------------------------------------------------
+  // -------------------------------------------------------------------------------
   // Atributos de la clase.
   // -------------------------------------------------------------------------------
   public teamId;
-  public dataTasks;
+  public dataTasks = [];
   public totalStates;
-
-  taskTitle: string;
-  taskDescription: string;
+  public createTasksForm: FormGroup;
+  public editTaskForm: FormGroup;
+  private idTask;
   taskDate: Date;
   categories: object = [];
 
@@ -37,13 +37,42 @@ export class BoardTaskTeamComponent implements OnInit {
     private toastrService: ToastrService,
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
-    private location: Location
-  ) { }
+    private location: Location,
+  ) {
+    this.teamId = this.route.snapshot.paramMap.get('id');
+  }
 
   ngOnInit(): void {
+    this.createTasksForm = this.formBuilder.group({
+      title: ['', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(40)
+      ]],
+      description: [''],
+      expiration_date: [''],
+      state_id: ['', [
+        Validators.required
+      ]],
+      team_id: [`${this.teamId}`]
+    });
+
+    this.editTaskForm = this.formBuilder.group({
+      title: ['', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(40)
+      ]],
+      description: [''],
+      expiration_date: [''],
+      state_id: [''],
+      team_id: ['', [
+        Validators.required,
+      ]]
+    });
+
     this.getCategories();
-    this.getTasks();
-    this.teamId = this.route.snapshot.paramMap.get('id');
+    this.getTasks(1);
     console.log(this.teamId);
   }
 
@@ -56,16 +85,35 @@ export class BoardTaskTeamComponent implements OnInit {
     });
   }
 
-  createTask(): void {
-    console.log(this.taskTitle);
-    console.log(this.taskDescription);
-    console.log(this.taskDate);
-    this.closeModal('create-task-modal');
+  // -------------------------------------------------------------------------------
+  // Getter de registerForm.
+  // -------------------------------------------------------------------------------
+  get registerState_id(): AbstractControl {
+    return this.createTasksForm.get('state_id');
   }
 
-  openModal(name: string): void {
+  get editState_id(): AbstractControl {
+    return this.editTaskForm.get('state_id');
+  }
+
+  // -------------------------------------------------------------------------------
+  // metodos de modal.
+  // -------------------------------------------------------------------------------
+  openModal(name: string, data?): void {
     const modal = document.getElementById(name);
     modal.style.display = 'block';
+
+    if (data) {
+      this.idTask = data.id;
+
+      this.editTaskForm.patchValue({
+        title: data.title,
+        description: data.description,
+        expiration_date: data.expiration_date,
+        state_id: data.state_id,
+        team_id: this.teamId
+      });
+    }
   }
 
   closeModal(name: string): void {
@@ -73,13 +121,14 @@ export class BoardTaskTeamComponent implements OnInit {
     modal.style.display = 'none';
   }
 
-  // Métodos CRUD de las tareas.
-  getTasks(): void {
-    this.appService.get(`team-tasks/${this.teamId}`).subscribe(
+  // -------------------------------------------------------------------------------
+  // CRUD del modal.
+  // -------------------------------------------------------------------------------
+  getTasks(state): void {
+    this.appService.get(`team-tasks/${this.teamId}/${state}`).subscribe(
       res => {
         this.dataTasks = res.data;
-        // this.totalStates = res.totalStates;
-        console.log(res.data);
+        this.totalStates = res.totalStates;
         if (this.dataTasks.length === (0 || null)) {
           this.toastrService.info('Has click en <strong>&quot;crear&quot;</strong> para crear una tarea', 'Sin tareas.', {
             progressBar: true,
@@ -97,5 +146,89 @@ export class BoardTaskTeamComponent implements OnInit {
         });
       }
     );
+  }
+
+  createTasks(): void {
+    console.log(this.createTasksForm.value);
+    console.log(this.teamId);
+    if (this.createTasksForm.value) {
+      this.appService.post('team-tasks', {
+        title: this.createTasksForm.value.title,
+        description: this.createTasksForm.value.description,
+        expiration_date: this.createTasksForm.value.expiration_date,
+        state_id: this.createTasksForm.value.state_id,
+        team_id: this.teamId
+      }).subscribe(
+        res => {
+          this.toastrService.success('', 'Tarea creada.', {
+            timeOut: 2000,
+            progressBar: true
+          });
+        },
+        error => {
+          console.log(error);
+          this.toastrService.error('error', 'Error con el servidor.', {
+            timeOut: 2000,
+            progressBar: true
+          });
+        }
+      );
+    } else {
+      this.toastrService.info('Rellene el formulario formularios', 'Sin datos.', {
+        timeOut: 2000,
+        progressBar: true
+      });
+    }
+
+    this.createTasksForm.reset();
+    this.closeModal('create-task-modal');
+  }
+
+  editTask(): void {
+    this.appService.put(`tasks/${this.idTask}`, {
+      title: this.editTaskForm.value.title,
+      description: this.editTaskForm.value.description,
+      expiration_date: this.editTaskForm.value.expiration_date,
+      state_id: this.editTaskForm.value.state_id,
+      team_id: this.teamId,
+    }).subscribe(
+      res => {
+        this.toastrService.success('', 'Tarea editada.', {
+          timeOut: 2000,
+          progressBar: true
+        });
+      },
+      error => {
+        console.log(error);
+        this.toastrService.error('Error con el servidor.', 'error', {
+          timeOut: 2000,
+          progressBar: true
+        });
+      }
+    );
+    this.closeModal('edit-task-modal');
+  }
+
+  deleteTask(): void {
+    this.appService.delete(`tasks/${this.idTask}`).subscribe(
+      res => {
+        console.log('entre a lo bueno');
+        this.toastrService.success('', 'Tarea eliminada.', {
+          timeOut: 2000,
+          progressBar: true
+        });
+      },
+      error => {
+        console.log('entre a lo malo');
+        console.log(error);
+        this.toastrService.error('Error con el servidor.', 'error al borrar', {
+          timeOut: 2000,
+          progressBar: true
+        });
+      }
+    );
+    this.closeModal('edit-task-modal');
+    this.closeModal('deleted-task-modal');
+    this.idTask = undefined;
   }
 }
